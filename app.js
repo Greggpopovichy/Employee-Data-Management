3eb2c4eb13a3e07c78485d367136274008e1eae3
Sangwon Song <ssong86@gatech.edu>
2017-09-23 13:54:53 -0400

    HTML styling not fitting to the table

--- app.js
+++ app.js
@@ -34,8 +34,41 @@ $("#add-employee").on("click", function(event){
 		startdate: emplyStartDate,
 		rate: emplyMonthlyRate
 	});
+});
 
-	
-    
+database.ref().on("child_added", function(snapshot) {
 
+      // Log everything that's coming out of snapshot
+
+      console.log(snapshot.val());
+
+      console.log(snapshot.val().name);
+
+      console.log(snapshot.val().role);
+
+      console.log(snapshot.val().startdate);
+
+      console.log(snapshot.val().rate);
+
+      $("#dynamic-row").append("<div class='well'><span id='name-display'> " + snapshot.val().name +
+        " </span><span id='role-display'> " + snapshot.val().role +
+        " </span><span id='date-display'> " + snapshot.val().date +
+        " </span><span id='monthsworked-display'> " + "" + " </span><span id='rate-display'>" + snapshot.val().rate +
+         "</span><span id='totalbilled-display>"+""+"</span></div>");
+
+      // Change the HTML to reflect
+
+      /*$("#name-display").text(snapshot.val().name);
+
+      $("#role-display").text(snapshot.val().role);
+
+      $("date-display").text(snapshot.val().startdate);
+
+      $("#rate-display").text(snapshot.val().rate);*/
+
+    // Handle the errors
+
+    }, function(errorObject) {
+
+      console.log("Errors handled: " + errorObject.code);
 });
