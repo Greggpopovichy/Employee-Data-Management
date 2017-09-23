9d2433ebfb001089a4823b909e583dba3bb5e029
Sangwon Song <ssong86@gatech.edu>
2017-09-23 11:28:21 -0400

    changes

--- app.js
+++ app.js
@@ -15,7 +15,27 @@ var database = firebase.database();
 var emplyName = "";
 var emplyRole = "";
 var emplyStartDate = "";
-var emplyMothsWorked = 0;
+var emplyMonthsWorked = 0;
 var emplyMonthlyRate = 0;
 var emplyBilled = 0;
 
+$("#add-employee").on("click", function(event){
+	event.preventDefault();
+
+	//grab data
+	emplyName = $("#name-input").val().trim();
+	emplyRole = $("#role-input").val().trim();
+	emplyStartDate= $("#date-input").val().trim();
+	emplyMonthlyRate = $("#rate-input").val().trim();
+
+	database.ref().push({
+		name: emplyName,
+		role: emplyRole,
+		startdate: emplyStartDate,
+		rate: emplyMonthlyRate
+	});
+
+	
+    
+
+});
